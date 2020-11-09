1718edd3c158dcd5d50553b911c33b76f0524003
ghklocal <57936354+hkngu@users.noreply.github.com>
2020-11-09 18:09:20 +0800

    by kun

--- src/top100/Demo22.java
+++ src/top100/Demo22.java
@@ -1,4 +1,35 @@
 package top100;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo22 {
+    private StringBuilder sb = new StringBuilder();
+    private ArrayList<String> list  = new ArrayList<>();
+    //深度优先遍历(dfs)的实践
+    public List<String> generateParenthesis(int n){
+        addStr(n,"(",0,0);
+        return list;
+    }
+
+    private void addStr(int n,String str,int left,int right){
+        //一条路径到头了，但是可能不是我们需要的结果
+        if (left == n && str.equals("("))
+            return;
+        if (right == left && str.equals(")"))
+            return;
+        sb.append(str);
+        if (str.equals("("))
+            left++;
+        else
+            right++;
+        if (right == n && left == n){
+            list.add(sb.toString());
+            sb.deleteCharAt(sb.length()-1);//删掉最后一个元素
+            return;
+        }
+        addStr(n,"(",left,right);
+        addStr(n,")",left,right);
+        sb.deleteCharAt(sb.length()-1);
+    }
 }
