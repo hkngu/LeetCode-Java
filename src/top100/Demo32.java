0db6237f179a73db9190cb5a4b277bb6c93b542f
ghklocal <57936354+hkngu@users.noreply.github.com>
2020-11-12 15:37:48 +0800

    by kun

--- src/top100/Demo32.java
+++ src/top100/Demo32.java
@@ -2,10 +2,11 @@ package top100;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Stack;
 
 public class Demo32 {
-    public int longestValidParentheses(String s){
+    static int longestValidParentheses(String s){
         if (s == null || s.length()<=0)
             return 0;
         char[] array = s.toCharArray();
@@ -21,11 +22,24 @@ public class Demo32 {
                 }
             }
         }
+        //找出最长连续数列的长度
+        int left= 0;
+        int right = 1;
+        int maxLen = 0;
         validList.sort(Comparator.naturalOrder());
-
+        System.out.println(validList.toString());
+        while (right<validList.size()){
+            if (validList.get(right) != validList.get(right-1)+1){
+                left = right;
+            }else {
+                maxLen= Math.max(maxLen,right-left+1);//找到连续的数列时，再计算长度，长度为right-left+1
+            }
+            right++;
+        }
+        return maxLen;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(longestValidParentheses(")(()())))"));
     }
 }
