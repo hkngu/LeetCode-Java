88c84ca9c10720092c38047b6464cb1d8af27fb2
ghklocal <57936354+hkngu@users.noreply.github.com>
2021-02-19 14:53:15 +0800

    by kun

--- src/algorithm/QuickSort.java
+++ src/algorithm/QuickSort.java
@@ -30,7 +30,7 @@ public class QuickSort {
     }
 
     public static void main(String[] args) {
-        int[]  arr = {1, 1, 2, 1, 3, 5, 3, 7, 9};
+        int[]  arr = {1,6,5,1,4,6,8};
         quickSort(arr,0,arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
